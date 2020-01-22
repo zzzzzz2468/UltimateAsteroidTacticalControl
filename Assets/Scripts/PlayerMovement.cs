b4fe2c1939fd47e365ad662bbcfaba28843e62e6
Elijah <enutter24@gmail.com>
2020-01-22 12:20:42 -0700

    Branch 1

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,13 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    public Rigidbody2D player;
+
+    public float rotateSpeed = 2.0f;
+    public float speed = 2.0f;
+
+    private float forwardInput;
+    private float rotateInput;
+
     void Start()
     {
-        
+        player.GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        forwardInput = Input.GetAxis("Vertical");
+        rotateInput = Input.GetAxis("Horizontal");
 
+    }
+
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        player.AddForce(transform.up * forwardInput * speed);
+
+        player.MoveRotation(player.rotation - rotateInput * rotateSpeed);
     }
 }
