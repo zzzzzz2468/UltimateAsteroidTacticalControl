fb13d40bf510d047ee7576d3d52df3fc11d9f966
Elijah <enutter24@gmail.com>
2020-01-22 13:29:17 -0700

    Changes and That

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject player;
+
+    void Start()
+    {
+        player.GetComponent<PlayerMovement>().ChangeMove(false);
+    }
+
+    void Update()
+    {
+        if (player.transform.position.x >= 11 || player.transform.position.x <= -11 || player.transform.position.y >= 5.2 || player.transform.position.y <= -5.2)
+        {
+            Death();
+        }
+    }
+
+    void Death()
+    {
+
+    }
+}
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,17 +8,31 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D player;
 
     //speed to have the player move display in editor
-    public float rotateSpeed = 2.0f;
-    public float speed = 2.0f;
+    public float rotateSpeed = 20.0f;
+    public float speed = 20.0f;
 
     //creates inputs variables
     private float forwardInput;
     private float rotateInput;
 
+    public GameObject FireCenter;
+    public GameObject FireLeft;
+    public GameObject FireRight;
+    public GameObject FireBack;
+
+    public GameObject Shield;
+    
+    private bool canMove = true;
+
     void Start()
     {
         //gets ths ridgidbody
         player.GetComponent<Rigidbody2D>();
+    }
+
+    public void ChangeMove(bool canMoveThing)
+    {
+        canMove = canMoveThing;
     }
 
     void Update()
@@ -28,20 +42,53 @@ public class PlayerMovement : MonoBehaviour
 
         //getting the horizontal axis
         rotateInput = Input.GetAxis("Horizontal");
-    }
 
-    void FixedUpdate()
-    {
-        //calls the movement in fixedupdate to avoid unequal movement because I do not use time.deltatime
-        Movement();
+        //calling the movement function
+        if(canMove)
+            Movement();
+
+        BackLights();
+
+        PowerUps();
     }
 
     void Movement()
     {
         //adds the movement to allow the player to move forward and back
-        player.AddForce(transform.up * forwardInput * speed);
+        player.AddForce(transform.up * forwardInput * speed * Time.deltaTime);
 
         //adds the players abilty to rotate left and right
-        player.MoveRotation(player.rotation - rotateInput * rotateSpeed);
+        player.MoveRotation(player.rotation - rotateInput * rotateSpeed * Time.deltaTime);
+    }
+
+    void BackLights()
+    {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.DownArrow))
+            FireCenter.SetActive(true);
+        else
+            FireCenter.SetActive(false);
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            FireLeft.SetActive(true);
+        else
+            FireLeft.SetActive(false);
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            FireRight.SetActive(true);
+        else
+            FireRight.SetActive(false);
+
+        if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+            FireBack.SetActive(true);
+        else
+            FireBack.SetActive(false);
+    }
+
+    void PowerUps()
+    {
+        if (Input.GetKey(KeyCode.Space))
+            Shield.SetActive(true);
+        else
+            Shield.SetActive(false);
     }
 }
