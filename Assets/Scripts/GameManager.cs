ade86d91d3f42272df9d28f4df8bb8c124965c32
Elijah <enutter24@gmail.com>
2020-01-25 20:56:16 -0700

    Player dies when leaves scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,21 +6,34 @@ public class GameManager : MonoBehaviour
 {
     public GameObject player;
 
+    private int lives = 3;
+
     void Start()
     {
-        player.GetComponent<PlayerMovement>().ChangeMove(false);
+
     }
 
     void Update()
     {
         if (player.transform.position.x >= 11 || player.transform.position.x <= -11 || player.transform.position.y >= 5.2 || player.transform.position.y <= -5.2)
         {
-            Death();
+            player.GetComponent<PlayerMovement>().ChangeMove(false);
+            player.transform.position = Vector2.zero;
+            player.SetActive(false);
+
+            if (lives >= 1)
+                Invoke("Death", 3);
+            else
+                Application.Quit();
         }
     }
 
     void Death()
     {
-
+        player.transform.rotation = Quaternion.Euler(0, 0, 0);
+        player.GetComponent<PlayerMovement>().ChangeMove(true);
+        player.SetActive(true);
+        lives -= 1;
+        print(lives);
     }
 }
